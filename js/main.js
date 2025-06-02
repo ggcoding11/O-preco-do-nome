4f4454625ddd2800ecf164dbb7901b77bbfc1d80
Sophia Machado <sophiamachadosilva07@gmail.com>
2025-06-02 19:08:54 -0300

    Index + main js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const config = {
+  type: Phaser.AUTO,
+  width: 1366,
+  height: 768,
+  physics: {
+    default: 'arcade',
+    arcade: { gravity: { y: 0 }, debug: false }
+  },
+  scene: [LoadingScene, TitleScene, EnredoScene, GameScene],
+};
+
+new Phaser.Game(config);
